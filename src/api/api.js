d4ab5461290ce0aa01372109ec3e7a7672b5e0a4
Amin <147066635+amin13m@users.noreply.github.com>
2025-11-14 04:28:58 -0800

    add axios api

---
+++ src/api/api.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { create } from "json-server";
+
+
+////CREATE////
+
+const api = axios.create({
+    baseURL: "http://localhost:3001",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    timeout: 5000
+});
+
+
+
+
+/////USER CRUD////
+
+export const UserAPI={
+    getAll: ()=> api.get("/users"),
+    getById: (id)=> api.get(`/users/${id}`),
+    create : (data) => api.post("/users", data),
+    update: (id, data) => api.put(`/users/${id}`, data),
+    delete: (id) => api.delete(`/users/${id}`)  
+}
+
+/////CATEGORY CRUD////
+
+export const CategoryAPI={
+    getAll: ()=> api.get("/categories"),
+    getById: (id)=> api.get(`/categories/${id}`),
+    create : (data) => api.post("/categories", data),
+    update: (id, data) => api.put(`/categories/${id}`, data),
+    delete: (id) => api.delete(`/categories/${id}`)
+}
+
+/////PRODUCT CRUD////
+
+export const ProductAPI={
+    getAll: ()=> api.get("/products"),
+    getById: (id)=> api.get(`/products/${id}`),
+    create : (data) => api.post("/products", data),
+    update: (id, data) => api.put(`/products/${id}`, data),
+    delete: (id) => api.delete(`/products/${id}`)
+}
+
+/////CART CRUD////
+
+export const CartAPI={
+    getAll: ()=> api.get("/carts"),
+    getById: (id)=> api.get(`/carts/${id}`),
+    create : (data) => api.post("/carts", data),
+    update: (id, data) => api.put(`/carts/${id}`, data),
+    delete: (id) => api.delete(`/carts/${id}`)
+}
+
+
+export default api
