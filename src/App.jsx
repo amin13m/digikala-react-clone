766a56b5b45c727a00bedc504cfa120525e26e5d
Amin <147066635+amin13m@users.noreply.github.com>
2025-11-14 04:27:01 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+ 
+  return (
+    <>
+      app
+    </>
+  )
+}
+
+export default App
